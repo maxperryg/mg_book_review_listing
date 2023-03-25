c86eef89796bf37d01aacbfbca391ad5a32b3f2d
Maxwell Grossman <maxwell.p.grossman@gmail.com>
2023-03-24 20:20:07 -0400

    initial ocmmit

---
+++ src/main/java/com/dotdash/recruiting/bookreview/Main.java
@@ -0,0 +1,10 @@
+package com.dotdash.recruiting.bookreview;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
